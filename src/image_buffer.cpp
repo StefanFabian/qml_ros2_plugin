b3a853ade3a69e76fb0ab8a5d040813696d641db
Stefan Fabian <github@stefanfabian.com>
2022-09-13 11:08:07 +0200

    Updated clang-format.

--- src/image_buffer.cpp
+++ src/image_buffer.cpp
@@ -17,14 +17,14 @@
 
 #include "qml_ros2_plugin/image_buffer.hpp"
 
-#if (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L) || __cplusplus >= 202002L
-#include <bit>
+#if ( defined( _MSVC_LANG ) && _MSVC_LANG >= 202002L ) || __cplusplus >= 202002L
+  #include <bit>
 namespace qml_ros2_plugin
 {
 namespace
 {
 using endian = std::endian;
-}
+} // namespace
 } // namespace qml_ros2_plugin
 #else
 namespace qml_ros2_plugin
@@ -34,44 +34,49 @@ namespace
 enum endian {
   little = 0,
   big = 1,
-#ifndef BYTEORDER_ENDIAN
-// Detect with GCC 4.6's macro.
-#   if defined(__BYTE_ORDER__)
-#       if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+  #ifndef BYTEORDER_ENDIAN
+    // Detect with GCC 4.6's macro.
+    #if defined( __BYTE_ORDER__ )
+      #if ( __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__ )
   native = little
-#       elif (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+      #elif ( __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__ )
   native = big
-#       else
-#           error "Unknown machine byteorder endianness detected. User needs to define BYTEORDER_ENDIAN."
-#       endif
-// Detect with GLIBC's endian.h.
-#   elif defined(__GLIBC__)
-#       include <endian.h>
-#       if (__BYTE_ORDER == __LITTLE_ENDIAN)
+      #else
+        #error "Unknown machine byteorder endianness detected. User needs to define BYTEORDER_ENDIAN."
+      #endif
+    // Detect with GLIBC's endian.h.
+    #elif defined( __GLIBC__ )
+      #include <endian.h>
+      #if ( __BYTE_ORDER == __LITTLE_ENDIAN )
   native = little
-#       elif (__BYTE_ORDER == __BIG_ENDIAN)
+      #elif ( __BYTE_ORDER == __BIG_ENDIAN )
   native = big
-#       else
-#           error "Unknown machine byteorder endianness detected. User needs to define BYTEORDER_ENDIAN."
-#       endif
-// Detect with _LITTLE_ENDIAN and _BIG_ENDIAN macro.
-#   elif defined(_LITTLE_ENDIAN) && !defined(_BIG_ENDIAN)
+      #else
+        #error "Unknown machine byteorder endianness detected. User needs to define BYTEORDER_ENDIAN."
+      #endif
+    // Detect with _LITTLE_ENDIAN and _BIG_ENDIAN macro.
+    #elif defined( _LITTLE_ENDIAN ) && !defined( _BIG_ENDIAN )
   native = little
-#   elif defined(_BIG_ENDIAN) && !defined(_LITTLE_ENDIAN)
+    #elif defined( _BIG_ENDIAN ) && !defined( _LITTLE_ENDIAN )
   native = big
-// Detect with architecture macros.
-#   elif defined(__sparc) || defined(__sparc__) || defined(_POWER) || defined(__powerpc__) || defined(__ppc__) || defined(__hpux) || defined(__hppa) || defined(_MIPSEB) || defined(_POWER) || defined(__s390__)
+    // Detect with architecture macros.
+    #elif defined( __sparc ) || defined( __sparc__ ) || defined( _POWER ) ||                       \
+        defined( __powerpc__ ) || defined( __ppc__ ) || defined( __hpux ) || defined( __hppa ) ||  \
+        defined( _MIPSEB ) || defined( _POWER ) || defined( __s390__ )
   native = big
-#   elif defined(__i386__) || defined(__alpha__) || defined(__ia64) || defined(__ia64__) || defined(_M_IX86) || defined(_M_IA64) || defined(_M_ALPHA) || defined(__amd64) || defined(__amd64__) || defined(_M_AMD64) || defined(__x86_64) || defined(__x86_64__) || defined(_M_X64) || defined(__bfin__)
+    #elif defined( __i386__ ) || defined( __alpha__ ) || defined( __ia64 ) ||                      \
+        defined( __ia64__ ) || defined( _M_IX86 ) || defined( _M_IA64 ) || defined( _M_ALPHA ) ||  \
+        defined( __amd64 ) || defined( __amd64__ ) || defined( _M_AMD64 ) ||                       \
+        defined( __x86_64 ) || defined( __x86_64__ ) || defined( _M_X64 ) || defined( __bfin__ )
   native = little
-#   elif defined(_MSC_VER) && (defined(_M_ARM) || defined(_M_ARM64))
+    #elif defined( _MSC_VER ) && ( defined( _M_ARM ) || defined( _M_ARM64 ) )
   native = little
-#   else
-#       error "Unknown machine byteorder endianness detected. User needs to define BYTEORDER_ENDIAN."
-#   endif
-#endif
+    #else
+      #error "Unknown machine byteorder endianness detected. User needs to define BYTEORDER_ENDIAN."
+    #endif
+  #endif
 };
-}
+} // namespace
 } // namespace qml_ros2_plugin
 #endif
 
