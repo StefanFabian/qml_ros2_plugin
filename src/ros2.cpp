28195f03256e3d8fef0c03bab8fd1f1536f3ee32
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-03-25 15:45:09 +0100

    Enable logger service for qml Node.

--- src/ros2.cpp
+++ src/ros2.cpp
@@ -64,6 +64,7 @@ void Ros2Qml::init( const QString &name, const QStringList &argv, quint32 )
   context_->init( argc, p_args.data() ); // TODO init options
   rclcpp::NodeOptions node_options;
   node_options.context( context_ );
+  node_options.enable_logger_service( true );
   node_ = rclcpp::Node::make_shared( name.toStdString(),
                                      node_options ); // TODO namespace and init options
   rclcpp::ExecutorOptions executor_options;
