bac250fad3c62f056f28fc55baf3d6cccdee33b9
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2026-02-13 20:48:52 +0100

    More control over time to be in line with qml6_ros2_plugin.

--- include/qml_ros2_plugin/time.hpp
+++ include/qml_ros2_plugin/time.hpp
@@ -34,27 +34,26 @@ Q_ENUM_NS( Ros2ClockTypes )
 /*!
  * Represents a point in time for a time source.
  *
- * Properties:
- *   - seconds: Floating point value containing the seconds passed since epoch. Depending on the
- * size of double, this may have significant precision loss. Use nanoseconds for precision.
- *   - nanoseconds: unsigned integer containing the nanoseconds since epoch.
- *   - clockType: The type of the time source. See Ros2ClockTypes
+ * Please note that this is a Q_GADGET and not a QObject which means there is no signal emitted
+ * when properties change.
  */
 class Time
 {
   Q_GADGET
-  //! The time in seconds (since 1970) as a decimal value. (Possible loss in precision)
-  Q_PROPERTY( double seconds READ seconds CONSTANT )
-  //! The time in nanoseconds (since 1970) as an unsigned integer.
-  Q_PROPERTY( quint64 nanoseconds READ nanoseconds CONSTANT )
-  //! The clock type of this time point. See Ros2ClockTypes
-  Q_PROPERTY( quint32 clockType READ clockType CONSTANT )
+  //! The time in seconds (since 1970) as an integer.
+  //! Use seconds() for floating point value including sub-seconds.
+  Q_PROPERTY( qint32 sec READ sec WRITE setSec )
+  //! The time in nanoseconds relative to the current second (as unsigned integer).
+  //! Use nanoseconds() for the full time in nanoseconds since epoch.
+  Q_PROPERTY( quint32 nanosec READ nanosec WRITE setNanosec )
 public:
   explicit Time( const rclcpp::Time &time = rclcpp::Time() ) : time_( time ) { }
 
-  double seconds() const { return time_.seconds(); }
+  //! Get the seconds since epoch as floating point including sub-seconds.
+  Q_INVOKABLE double seconds() const { return time_.seconds(); }
 
-  quint64 nanoseconds() const { return time_.nanoseconds(); }
+  //! Get the nanoseconds since epoch.
+  Q_INVOKABLE qint64 nanoseconds() const { return time_.nanoseconds(); }
 
   //! Whether the time represented by this instance is zero.
   Q_INVOKABLE bool isZero() const { return nanoseconds() == 0; }
@@ -64,9 +63,28 @@ public:
   //!  are lost. The time is always rounded down to prevent the JS Date from being in the future.
   Q_INVOKABLE QDateTime toJSDate() const { return rosToQmlTime( time_ ); }
 
-  quint32 clockType() const { return time_.get_clock_type(); }
+  //! The type of the time source. See Ros2ClockTypes
+  Q_INVOKABLE quint32 clockType() const { return time_.get_clock_type(); }
 
   rclcpp::Time getTime() const { return time_; }
+
+  qint32 sec() const { return builtin_interfaces::msg::Time( time_ ).sec; }
+
+  void setSec( qint32 sec )
+  {
+    auto t = builtin_interfaces::msg::Time( time_ );
+    t.sec = sec;
+    time_ = rclcpp::Time( t );
+  }
+
+  quint32 nanosec() const { return builtin_interfaces::msg::Time( time_ ).nanosec; }
+
+  void setNanosec( quint32 nanosec )
+  {
+    auto t = builtin_interfaces::msg::Time( time_ );
+    t.nanosec = nanosec;
+    time_ = rclcpp::Time( t );
+  }
 
 private:
   rclcpp::Time time_;
@@ -75,19 +93,18 @@ private:
 /*!
  * Represents a duration for a time source.
  *
- * Properties:
- *   - seconds: Floating point value containing the seconds passed between two time points. Can be
- *     negative. Depending on the size of double, may have significant precision loss. Use
- *     nanoseconds if you need a precise measurement.
- *   - nanoseconds: Signed integer containing the nanoseconds that have passed between two timepoints.
+ * Please note that this is a Q_GADGET and not a QObject which means there is no signal emitted
+ * when properties change.
  */
 class Duration
 {
   Q_GADGET
-  //! The time in seconds that has passed between two timepoints. (Possible loss in precision)
-  Q_PROPERTY( double seconds READ seconds CONSTANT )
-  //! The duration in nanoseconds
-  Q_PROPERTY( qint64 nanoseconds READ nanoseconds CONSTANT )
+  //! The duration in seconds as an integer.
+  //! Use seconds() for floating point value including sub-seconds.
+  Q_PROPERTY( qint32 sec READ sec WRITE setSec )
+  //! The time in nanoseconds relative to the current second (as unsigned integer).
+  //! Use nanoseconds() for the full duration in nanoseconds.
+  Q_PROPERTY( quint32 nanosec READ nanosec WRITE setNanosec )
 public:
   explicit Duration(
       const rclcpp::Duration &duration = rclcpp::Duration( std::chrono::nanoseconds( 0 ) ) )
@@ -95,9 +112,11 @@ public:
   {
   }
 
-  double seconds() const { return duration_.seconds(); }
+  //! Get the seconds of the duration as floating point including sub-seconds.
+  Q_INVOKABLE double seconds() const { return duration_.seconds(); }
 
-  qint64 nanoseconds() const { return duration_.nanoseconds(); }
+  //! Get the full duration as nanosecond.
+  Q_INVOKABLE qint64 nanoseconds() const { return duration_.nanoseconds(); }
 
   //! Whether the duration represented by this instance is zero.
   Q_INVOKABLE bool isZero() const { return nanoseconds() == 0; }
@@ -107,6 +126,24 @@ public:
   Q_INVOKABLE double toJSDuration() const { return rosToQmlDuration( duration_ ); }
 
   rclcpp::Duration getDuration() const { return duration_; }
+
+  qint32 sec() const { return builtin_interfaces::msg::Duration( duration_ ).sec; }
+
+  void setSec( qint32 sec )
+  {
+    auto d = builtin_interfaces::msg::Duration( duration_ );
+    d.sec = sec;
+    duration_ = rclcpp::Duration( d );
+  }
+
+  quint32 nanosec() const { return builtin_interfaces::msg::Duration( duration_ ).nanosec; }
+
+  void setNanosec( quint32 nanosec )
+  {
+    auto d = builtin_interfaces::msg::Duration( duration_ );
+    d.nanosec = nanosec;
+    duration_ = rclcpp::Duration( d );
+  }
 
 private:
   rclcpp::Duration duration_;
