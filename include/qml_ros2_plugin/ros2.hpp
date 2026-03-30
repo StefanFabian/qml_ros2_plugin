9817d6c3fef12b420598109429a8db1758752ebd
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2026-03-30 15:36:55 +0200

    Small documentation fixes. (#15)
    * Small documentation fixes.
    
    * Switch to ros2 testing sources for CI to get newest packages.

--- include/qml_ros2_plugin/ros2.hpp
+++ include/qml_ros2_plugin/ros2.hpp
@@ -220,7 +220,7 @@ public:
   //! Creates a QoS wrapper with the settings for SystemDefaults from rclcpp.
   Q_INVOKABLE qml_ros2_plugin::QoSWrapper SystemDefaultsQoS();
 
-  //! @copydoc Ros2Qml::isRosInitialized
+  //! @copydoc Ros2Qml::isInitialized
   Q_INVOKABLE bool isInitialized() const;
 
   //! @copydoc Ros2Qml::init(const QString &, Ros2InitOptions)
@@ -330,10 +330,10 @@ public:
                                         quint32 queue_size = 10 );
 
   /*!
-   * Creates a Subscriber to createSubscription to ROS messages.
+   * Creates a Subscriber to subscribe to ROS messages.
    * Convenience function to create a subscriber in a single line.
    *
-   * @param topic The topic to createSubscription to.
+   * @param topic The topic to subscribe to.
    * @param qos The QoS settings for the subscription.
    * @return A Subscriber instance.
    */
@@ -347,10 +347,10 @@ public:
   Q_INVOKABLE QObject *createSubscription( const QString &topic, quint32 queue_size = 1 );
 
   /*!
-   * Creates a Subscriber to createSubscription to ROS messages.
+   * Creates a Subscriber to subscribe to ROS messages.
    * Convenience function to create a subscriber in a single line.
    *
-   * @param topic The topic to createSubscription to.
+   * @param topic The topic to subscribe to.
    * @param message_type The type of the messages to subscribe to on the topic.
    * @param queue_size The maximum number of incoming messages to be queued for processing.
    * @return A Subscriber instance.
